cfc23b0f650dd1382af2a06fd85fff36d8b472b8
tolozine <tolozine@163.com>
2025-02-21 13:11:26 +0800

    Update SSW.py
    修改快捷键以及用插件方式管理

--- SSW.py
+++ SSW.py
@@ -1,66 +1,83 @@
+
+bl_info = {
+    "name": "Blender Switch Scene",
+    "blender": (3, 0, 0),
+    "category": "Render",
+    "version": (1, 0),
+    "author": "tolozine",
+    "description": "通过shift+alt+S 调出场景列表菜单,进行快速切换",
+}
+
+
 import bpy
+
+# 操作符：执行场景切换
 class SCENE_OT_Switch(bpy.types.Operator):
-    """切换场景操作"""
     bl_idname = "scene.switch"
-    bl_label = "切换场景"
-    
+    bl_label = "Switch Scene"
     scene_name: bpy.props.StringProperty()
-    
+
     def execute(self, context):
-        if self.scene_name in bpy.data.scenes:
-            context.window.scene = bpy.data.scenes[self.scene_name]
-            return {'FINISHED'}
-        self.report({'ERROR'}, "场景不存在")
-        return {'CANCELLED'}
+        scene = bpy.data.scenes.get(self.scene_name)
+        if scene:
+            context.window.scene = scene
+        return {'FINISHED'}
+
+# 菜单类：显示所有场景
 class SCENE_MT_SwitchMenu(bpy.types.Menu):
-    """场景切换菜单"""
     bl_idname = "SCENE_MT_switch_menu"
-    bl_label = "选择场景"
-    
+    bl_label = "Switch Scene"
+
     def draw(self, context):
         layout = self.layout
         for scene in bpy.data.scenes:
-            op = layout.operator(SCENE_OT_Switch.bl_idname, text=scene.name)
-            op.scene_name = scene.name
+            layout.operator("scene.switch", text=scene.name).scene_name = scene.name
+
+# 操作符：触发场景菜单显示
 class SCENE_OT_OpenSceneMenu(bpy.types.Operator):
-    """打开场景菜单"""
     bl_idname = "scene.open_scene_menu"
-    bl_label = "打开场景菜单"
-    
+    bl_label = "Open Scene Menu"
+    bl_description = "Open a menu to switch scenes"
+
     def execute(self, context):
         bpy.ops.wm.call_menu(name=SCENE_MT_SwitchMenu.bl_idname)
         return {'FINISHED'}
-# 快捷键注册
-addon_keymaps = []
-def register():
-    bpy.utils.register_class(SCENE_OT_Switch)
-    bpy.utils.register_class(SCENE_MT_SwitchMenu)
-    bpy.utils.register_class(SCENE_OT_OpenSceneMenu)
-    
-    # 配置快捷键
+
+# 注册快捷键
+def register_keymap():
     wm = bpy.context.window_manager
-    kc = wm.keyconfigs.addon
-    if kc:
-        km = kc.keymaps.new(name='Window', space_type='EMPTY')
-        kmi = km.keymap_items.new(
-            SCENE_OT_OpenSceneMenu.bl_idname, 
-            'S', 
-            'PRESS', 
-            ctrl=True, 
-            shift=True
-        )
-        addon_keymaps.append((km, kmi))
+    keymap = wm.keyconfigs.addon.keymaps.new(name='Window', space_type='EMPTY')
+    keymap_items = keymap.keymap_items.new(SCENE_OT_OpenSceneMenu.bl_idname, 'S', 'PRESS', shift=True, alt=True)
+    return keymap, keymap_items
+
+# 注销快捷键
+def unregister_keymap():
+    wm = bpy.context.window_manager
+    keymap = wm.keyconfigs.addon.keymaps.get('Window')
+    if keymap:
+        for kmi in keymap.keymap_items:
+            if kmi.idname == SCENE_OT_OpenSceneMenu.bl_idname:
+                keymap.keymap_items.remove(kmi)
+                break
+
+# 注册/注销类和快捷键
+classes = (
+    SCENE_OT_Switch,
+    SCENE_MT_SwitchMenu,
+    SCENE_OT_OpenSceneMenu,
+)
+
+def register():
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    # 注册快捷键
+    register_keymap()
+
 def unregister():
-    for km, kmi in addon_keymaps:
-        km.keymap_items.remove(kmi)
-    addon_keymaps.clear()
-    
-    bpy.utils.unregister_class(SCENE_OT_OpenSceneMenu)
-    bpy.utils.unregister_class(SCENE_MT_SwitchMenu)
-    bpy.utils.unregister_class(SCENE_OT_Switch)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    # 注销快捷键
+    unregister_keymap()
+
 if __name__ == "__main__":
     register()
-# 使用说明：
-# 1. 将脚本粘贴到Blender文本编辑器中并运行
-# 2. 使用 Ctrl + Shift + S 打开场景菜单
-# 3. 从列表中选择需要切换的场景
